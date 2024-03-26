4b025f90ffc1be5ec6eb77aa39e9847c424a76dd
Giovanni vignone <giovannivignone@Giovannis-MacBook-Pro.local>
2024-03-26 01:59:15 -0400

    compiling w/o env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-toolbox";
-require("dotenv").config();
 
 const config: HardhatUserConfig | any = {
-  defaultNetwork: "sepolia",
   solidity: {
     version: "0.8.20",
     settings: {
@@ -13,19 +11,6 @@ const config: HardhatUserConfig | any = {
         runs: 800,
       },
     },
-  },
-  networks: {
-    sepolia: {
-      url: process.env.SEPOLIA_RPC || "",
-      accounts: process.env.PK !== undefined ? [process.env.PK] : [],
-    },
-    mainnet: {
-      url: process.env.MAINNET_RPC || "",
-      accounts: process.env.PK !== undefined ? [process.env.PK] : [],
-    },
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_KEY,
   },
   sourcify: {
     // Disabled by default
